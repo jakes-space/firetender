13683a3caf384f363a034e1bf3f68343a6fca6f3
Jake Hartman <jakehartman@gmail.com>
2022-10-31 20:38:02 -0700

    Renamed to FireTender; rollup stuff is a work in progress

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  rootDir: ".",
-};
