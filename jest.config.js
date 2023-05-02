7c89993a8e1ba8b4a30a7e19425644b0f68edb89
Jake Hartman <jakehartman@gmail.com>
2023-05-02 12:43:32 -0700

    Ignore firestore-deps-{admin,web}.ts when calculating coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   rootDir: ".",
   testRegex: "(\\.|/)(test|spec)\\.[jt]sx?$",
   collectCoverage: true,
@@ -11,6 +11,8 @@ module.exports = {
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "/__tests__/",
+    "/src/firestore-deps-admin.ts",
+    "/src/firestore-deps-web.ts",
     "/src/index.ts",
   ],
 };
