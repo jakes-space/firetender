2d5baf651296cab8922a8a9b95d2bb500ea0ca70
Jake Hartman <jakehartman@gmail.com>
2023-08-15 11:11:46 -0700

    Remove unused TS helper type

--- src/ts-helpers.ts
+++ src/ts-helpers.ts
@@ -15,13 +15,3 @@ export type DeepReadonly<T> = T extends Array<infer ArrKey>
   : T extends Record<any, unknown>
   ? { readonly [ObjKey in keyof T]: DeepReadonly<T[ObjKey]> }
   : T;
-
-export type MakeFieldsWithDefaultsOptional<T> = T extends Array<infer ArrKey>
-  ? ReadonlyArray<DeepReadonly<ArrKey>>
-  : T extends Map<infer MapKey, infer MapVal>
-  ? ReadonlyMap<DeepReadonly<MapKey>, DeepReadonly<MapVal>>
-  : T extends Set<infer SetKey>
-  ? ReadonlySet<DeepReadonly<SetKey>>
-  : T extends Record<any, unknown>
-  ? { readonly [ObjKey in keyof T]: DeepReadonly<T[ObjKey]> }
-  : T;
