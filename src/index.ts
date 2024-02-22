86c7a01a1fb91686a4503f65e3ef7eaef8f1aed9
Jake Hartman <jakehartman@gmail.com>
2024-02-22 06:29:18 -0800

    Export type Firestore

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export {
   FiretenderIOError,
   FiretenderUsageError,
 } from "./errors";
-export { Timestamp, where } from "./firestore-deps";
+export { type Firestore, Timestamp, where } from "./firestore-deps";
 export { FiretenderCollection } from "./FiretenderCollection";
 export { FiretenderDoc, type FiretenderDocOptions } from "./FiretenderDoc";
 export {
